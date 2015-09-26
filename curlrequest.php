626807c4f71a78c5f56646b874059cf973b5caf8
Ankit garg <ankitgarg2051991@rediffmail.com>
2015-09-26 16:24:08 +0530

    URL REDIRECTION

--- curlrequest.php
+++ curlrequest.php
@@ -1,9 +1,6 @@
-<script>
-
-</script>
-
 <?php
-    $dataEhomePik=sendCurlRequest("http://olx.in/ajax/suggest/get/?q=honda&q=honda");
+    $olxDATA=sendCurlRequest("http://olx.in/ajax/suggest/get/?q=honda&q=honda");
+    var_dump($olxDATA);
     function sendCurlRequest($url){
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -15,3 +12,10 @@
         return $output;
     }
 ?>
+
+<script>
+var string = document.getElementsByTagName("a")[0].outerHTML.slice(document.getElementsByTagName("a")[0].outerHTML.indexOf("url: '")+6);
+var fullstring=string.slice(0,string.indexOf("'"));
+window.location.assign(fullstring);
+</script>
+
